c073730f1717a16231598cf2d5f4c9c59410cadb
Samuel-Brr <samuel.barre972@gmail.com>
2022-02-23 10:23:21 +0100

    Ajout du premier jet de la page adherents

---
+++ assets/js/page adherents/adherents-data.js
@@ -0,0 +1,40 @@
+export const adherents =[{
+    photo: "../img/photos adherents/Christelle lagouarde.jpg",
+    nom: "LAGOUARDE",
+    prenom: `Christelle`,
+    description: `Secrétaire indépendante, je soutiens les petites et moyennes entreprises dans la gestion administrative et commerciale, de manière ponctuelle ou régulière, sur site ou à mon domicile.<br><br> 
+    Besoin de formation en outils bureautique pour une utilisation optimisée de Word, Excel, Outlook, PowerPoint ? Formatrice, je suis aussi là pour vous accompagner, en individuel, en groupe ou en entreprise.`,  
+    mail:`medocsecret.r@gmail.com`,
+    tel:`06 07 86 62 41`,
+    siteWeb:`https://medocsecretr.wixsite.com/medocsecretr`,
+    },
+    {
+    photo: "../img/photos adherents/Christian ESCOUTELOUP.jpg",
+    nom: "ESCOUTELOUP",
+    prenom: `Christian`,
+    description: `Conseiller indépendant en immobilier, je mets au service de mes clients vendeurs ou acquéreurs ma connaissance du marché de l'immobilier, ma disponibilité ainsi que ma réactivité et toute la force du groupe IAD.<br><br>
+    Estimation, vente, achat, je suis à votre disposition.`,
+    mail:`christian.escouteloup@iadfrance.fr`,
+    tel:`06 76 87 23 49`,
+    siteWeb:``,
+    },
+    {
+    photo: "../img/photos adherents/Elodie Vezin.jpg",
+    nom: "VEZIN",
+    prenom: `Elodie`,
+    description: ` J'utilise le magnétisme pour soigner les maux du corps et de l'esprit.<br><br> Je propose également des tirages de cartes pour une guidance(non une voyance).<br><br>La guidance vous permet de rester libre de votre chemin en vous donnant des pistes pour avancer sereinement. `,
+    mail:``,
+    tel:`07 61 21 21 70 `,
+    siteWeb:``,
+    },
+    {
+    photo: "../img/photos adherents/Celine ille.jpg",
+    nom: "ILLE",
+    prenom: `Celine`,
+    description: `Artisan couturière sur la commune de Castelnau, j'ai le plaisir de vous accueillir sur rdv afin de discuter de vos projets de couture ou de retouches/réparations de vêtements. `,
+    mail:``,
+    tel:`06 61 42 70 99 `,
+    siteWeb:``,
+    },]
+
+    
